4bee15f03543dba1e96955a80306c9c9b4f7c00e
Aman Dhakad <dhakadaman6@gmail.com>
2022-08-21 15:46:08 +0530

    valid moves using chessjs lib, code improved, more improvements needed

--- src/index.js
+++ src/index.js
@@ -3,120 +3,80 @@ import ReactDOM from 'react-dom';
 import Square from './square';
 import './index.css';
 
-//importing availableMoves
-import availableMoves from './availableMoves.js';
-
+import { Chess } from 'chess.js';
 
 class Board extends React.Component {
 
 	constructor(props) {
 		super(props);
-		let starting_position = Array(64).fill(null);
-		//starting position pieces here
-		for (var i = 0; i < starting_position.length; i++) {
-			if (i>7 && i<16) {
-				starting_position[i] = "bP";
-			}
-			if (i>47 && i<56) {
-				starting_position[i] = "wP";
-			}
-			starting_position[0] = "bR";
-			starting_position[1] = "bN";
-			starting_position[2] = "bB";
-			starting_position[3] = "bQ";
-			starting_position[4] = "bK";
-			starting_position[5] = "bB";
-			starting_position[6] = "bN";
-			starting_position[7] = "bR";
 
-			starting_position[56] = "wR";
-			starting_position[57] = "wN";
-			starting_position[58] = "wB";
-			starting_position[59] = "wQ";
-			starting_position[60] = "wK";
-			starting_position[61] = "wB";
-			starting_position[62] = "wN";
-			starting_position[63] = "wR";
+		//we will get the fen on every move from library
+		const chess = new Chess();
+		const fen = chess.fen();
+		const board = chess.board();
+
+		let startingPieces = this.makePiecesArray(board);
+		this.state = {pieces: startingPieces, fen: fen, clicked: null, available_moves: [], isFlipped: false};
+	}
+
+	makePiecesArray(board) {
+		let pieces = Array(64).fill(null);
+
+		//row loop
+		for(let i=0; i < board.length; i++) {
+			//square loop
+			for(let j=0; j<board[i].length; j++) {
+				let pieceValue = board[i][j] ? (board[i][j].color + board[i][j].type.toUpperCase()): null;
+
+				//superIndex is index of allpieces from 0 to 63
+				let superIndex = (i*8)+j;
+				pieces[superIndex] = pieceValue;
+			}
 		}
-		//updating
-		//pieces contain the piece name array
 
-		//clicked contain the id of square that is currently clicked or not
-		this.state = {pieces: starting_position, clicked: null, available_moves: [], isFlipped: false};
+		return pieces;
+	}
+
+	squareIndexToNotation(index) {
+		let row = Math.floor(index/8);
+		let column = index%8;
+
+		let letter = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h"}
+
+		return (letter[column])+((8-row).toString());
 	}
 
 	flipTheBoard() {
 		this.setState({isFlipped: !(this.state.isFlipped)});
 	}
 
-	checkLegalMove(from, to) {
-		//this function will check legality of move
-		//return true for now
-		return true;
-	}
-
-	makeMove(from, to) {
-			//check legal move
-			let new_state = {...this.state};
-
-			//stop if square selected is empty
-			//**later this if to be handled by checkLegalMove**
-			if (this.state.pieces[from]==null) {
-				new_state.clicked = to;
-				this.setState(new_state);
-				return 0;
-			}
-
-			//first checking legal move or not
-			if (this.checkLegalMove(from, to)) {
-
-				//moving the piece
-				new_state.pieces[to] = this.state.pieces[from];
-				new_state.pieces[from] = null;
-
-				//set state and return
-				this.setState(new_state);
-				return 1;
-
-			}
-			return 0;
+	makeMove(to) {
+			const chess = new Chess();
+			chess.load(this.state.fen);
+			chess.move({ from: this.squareIndexToNotation(this.state.clicked), to: this.squareIndexToNotation(to) });
+			const fen = chess.fen();
+			const board = chess.board();
+			let piecesArray = this.makePiecesArray(board);
+			this.setState({pieces: piecesArray, fen: fen, clicked: null});
+			return true;
 	}
 
 	//updating state on clicking square component
 	handleClick(i) {
-
-		let new_state = {...this.state};
-
 		//if no square is clicked, simply focus
-		if (new_state.clicked==null) {
-			new_state.clicked = i;
-			this.setState(new_state);
+		if (this.state.clicked==null) {
+			this.setState({clicked: i});
 			return;
 		}
 
-		//unfocusing if clicked focused square else make a move:
-		if (new_state.clicked===i) {
-
-			new_state.clicked = null;
-			this.setState(new_state);
+		//unfocus if clicked focused square else make a move:
+		if (this.state.clicked===i) {
+			this.setState({clicked: null});
 			return;
-
 		} else {
-			//move will be made here
-			//vars
-			let from = new_state.clicked;
+			let from = this.state.clicked;
 			let to = i;
-
-			//calling
-			let move_response;
-			move_response = this.makeMove(from, to);
-
-			//unfocusing if move success
-			if (move_response===1) {
-				let new_state2 = {...this.state};
-				new_state2.clicked = null;
-				this.setState(new_state2);
-			}
+			this.makeMove(to);
 			return;
 		}
 	}
@@ -144,13 +104,12 @@ class Board extends React.Component {
 		if(this.state.isFlipped) {
 			boxes.reverse();
 		}
-		
+
 		return (<div className="chess-row" key={`row`+firstIndex}>{boxes}</div>);
 	}
 
 	makeBoard() {
 		let rows = [];
-
 		for(let i = 0; i < 8; i++) {
 			rows.push(this.renderRow(i*8));
 		}
